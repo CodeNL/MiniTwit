9094469397f44548f3cddbeef0111cd081e6a717
Mark Stacey <markjstacey@gmail.com>
2017-07-05 16:05:17 -0230

    Remove user ids, use username as primary key

--- minitwit/minitwit.py
+++ minitwit/minitwit.py
@@ -29,22 +29,6 @@ app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
 all_users = {}
 all_messages = []
 
-def get_next_user_id():
-    """Generate new user id."""
-    max_id = 0
-
-    for user_id in all_users:
-        if int(user_id) > max_id:
-            max_id = int(user_id)
-    return str(max_id + 1)
-
-def get_user_id(username):
-    """Convenience method to look up the id for a username."""
-    for user_id, user in all_users.items():
-        if user['username'] == username:
-            return user_id
-    return None
-
 
 def format_datetime(timestamp):
     """Format a timestamp for display."""
@@ -59,10 +43,9 @@ def robohash(username, size=80):
 
 @app.before_request
 def before_request():
-    g.current_user_id = None
-    if 'user_id' in session and session['user_id'] in all_users:
-        g.current_user = all_users[session['user_id']]
-        g.current_user_id = session['user_id']
+    g.current_username = None
+    if 'username' in session and session['username'] in all_users:
+        g.current_username = session['username']
 
 
 @app.route('/')
@@ -71,11 +54,11 @@ def timeline():
     redirect to the public timeline.  This timeline shows the user's
     messages as well as all the messages of followed users.
     """
-    if not g.current_user_id:
+    if not g.current_username:
         return redirect(url_for('public_timeline'))
     messages = []
     for message in all_messages:
-        if message['author_id'] == g.current_user_id or message['author_id'] in all_users[g.current_user_id]['following']:
+        if message['author'] == g.current_username or message['author'] in all_users[g.current_username]['following']:
             messages.append(message)
     return render_template('timeline.html', messages=messages)
 
@@ -89,29 +72,27 @@ def public_timeline():
 @app.route('/<username>')
 def user_timeline(username):
     """Display's a users tweets."""
-    whom_id = get_user_id(username)
-    if whom_id is None:
+    if not username in all_users:
         abort(404)
     followed = False
-    if g.current_user_id and whom_id in all_users[g.current_user_id]['following']:
+    if g.current_username and username in all_users[g.current_username]['following']:
         followed = True
     messages = []
     for message in all_messages:
-        if message['author_id'] == whom_id:
+        if message['author'] == username:
             messages.append(message)
     return render_template('user_timeline.html', messages=messages, followed=followed,
-            profile_user=all_users[whom_id])
+            profile_username=username)
 
 
 @app.route('/<username>/follow')
 def follow_user(username):
     """Adds the current user as follower of the given user."""
-    if not g.current_user_id:
+    if not g.current_username:
         abort(401)
-    whom_id = get_user_id(username)
-    if whom_id is None:
+    if not username in all_users:
         abort(404)
-    all_users[g.current_user_id]['following'].append(whom_id)
+    all_users[g.current_username]['following'].append(username)
     flash('You are now following "%s"' % username)
     return redirect(url_for('user_timeline', username=username))
 
@@ -119,12 +100,11 @@ def follow_user(username):
 @app.route('/<username>/unfollow')
 def unfollow_user(username):
     """Removes the current user as follower of the given user."""
-    if not g.current_user_id:
+    if not g.current_username:
         abort(401)
-    whom_id = get_user_id(username)
-    if whom_id is None:
+    if not username in all_users:
         abort(404)
-    all_users[g.current_user_id]['following'].remove(whom_id)
+    all_users[g.current_username]['following'].remove(username)
     flash('You are no longer following "%s"' % username)
     return redirect(url_for('user_timeline', username=username))
 
@@ -132,14 +112,13 @@ def unfollow_user(username):
 @app.route('/add_message', methods=['POST'])
 def add_message():
     """Registers a new message for the user."""
-    if not g.current_user_id:
+    if not g.current_username:
         abort(401)
     if request.form['text']:
         all_messages.insert(0, {
-            'author_id': g.current_user_id,
+            'author': g.current_username,
             'text': request.form['text'],
             'pub_date': int(time.time()),
-            'username': all_users[g.current_user_id]['username']
         })
         flash('Your message was recorded')
     return redirect(url_for('timeline'))
@@ -148,22 +127,19 @@ def add_message():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """Logs the user in."""
-    if g.current_user_id:
+    if g.current_username:
         return redirect(url_for('timeline'))
     error = None
     if request.method == 'POST':
-        current_user_id = None
-        for user_id, user in all_users.items():
-            if user['username'] == request.form['username']:
-                current_user_id = user_id
-        if current_user_id is None:
+        current_username = request.form['username']
+        if not current_username in all_users:
             error = 'Invalid username'
-        elif not check_password_hash(all_users[current_user_id]['pw_hash'],
+        elif not check_password_hash(all_users[current_username]['pw_hash'],
                                      request.form['password']):
             error = 'Invalid password'
         else:
             flash('You were logged in')
-            session['user_id'] = current_user_id
+            session['username'] = current_username
             return redirect(url_for('timeline'))
     return render_template('login.html', error=error)
 
@@ -171,7 +147,7 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     """Registers the user."""
-    if g.current_user_id:
+    if g.current_username:
         return redirect(url_for('timeline'))
     error = None
     if request.method == 'POST':
@@ -181,12 +157,11 @@ def register():
             error = 'You have to enter a password'
         elif request.form['password'] != request.form['password2']:
             error = 'The two passwords do not match'
-        elif get_user_id(request.form['username']) is not None:
+        elif request.form['username'] in all_users:
             error = 'The username is already taken'
         else:
-            user_id = get_next_user_id()
-            all_users[user_id] = {
-                'username': request.form['username'],
+            username = request.form['username']
+            all_users[username] = {
                 'pw_hash': generate_password_hash(request.form['password']),
                 'following': []
             }
@@ -199,7 +174,7 @@ def register():
 def logout():
     """Logs the user out."""
     flash('You were logged out')
-    session.pop('user_id', None)
+    session.pop('username', None)
     return redirect(url_for('public_timeline'))
 
 
